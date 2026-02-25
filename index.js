4ba57cdf4097e8c2083dbc7be5c4ed9b06800eb6
Vinzee <xploitforceofficial@gmail.com>
2026-02-25 19:05:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -649,4 +649,8 @@ client.on("messageCreate", async message => {
   }
 });
 
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
 client.login(process.env.TOKEN);
